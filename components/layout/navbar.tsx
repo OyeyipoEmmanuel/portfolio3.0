c4ac4468c864e42e4fc21a4a87ded8fff75f3081
Emmanuel <emmasco0207@gmail.com>
2025-12-25 00:19:26 +0100

    about section

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -2,7 +2,7 @@
 
 const Navbar = () => {
   return (
-    <nav className="fixed w-[80%] md:w-[30%] h-10 top-6 bg-clip-padding backdrop-filter inset-x-0 mx-auto backdrop-blur-[7px] bg-opacity-0 backdrop-saturate-100 backdrop-contrast-100 border border-gray-300 rounded-xl">
+    <nav className="fixed z-50 w-[80%] md:w-[30%] h-10 top-6 bg-clip-padding backdrop-filter inset-x-0 mx-auto backdrop-blur-[7px] bg-opacity-0 backdrop-saturate-100 backdrop-contrast-100 border border-gray-300 rounded-xl">
         hello
     </nav>
   )
