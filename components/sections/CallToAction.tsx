9184b14b26dd8e1401a9dc9bc560a6f6825ffd03
Emmanuel <emmasco0207@gmail.com>
2025-12-29 23:14:58 +0100

    feat: completed cta, footer and experience pg

---
+++ components/sections/CallToAction.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { SiGmail } from 'react-icons/si'
+import { IoLogoWhatsapp } from "react-icons/io";
+
+
+const CallToAction = () => {
+    return (
+        <main className="mt-24 bg-[url('/assets/images/background2.webp')] bg-cover bg-repeat-space bg-center rounded-md">
+            <section className='flex items-center justify-center mx-auto min-h-[300px]'>
+                <div>
+                    <h1 className='text-white font-semibold tracking-tighter text-2xl text-center md:text-4xl font-barlow'>You've come a long way!</h1>
+                    <h3 className='text-white/80 text-center mt-2 text-xl tracking-tight font-barlow'>Like what you see?</h3>
+
+                    <div className='flex flex-col md:flex-row items-center space-y-4 w-full md:space-y-0 md:space-x-6 justify-center mt-16'>
+
+                        <a href='mailto:deleoth702@gmail.com?subject=From%20your%20portfolio' target="_blank">
+                            <button className='flex items-center space-x-2 text-lg bg-white px-12 md:px-5 py-2 hover:scale-105 duration-300 transition-all cursor-pointer'>
+                                <p>Send me an</p>
+                                <SiGmail />
+                            </button>
+                        </a>
+
+                        <a href="https://wa.me/2348111184177?text=Hello%20Emmanuel,%20I%20checked%20your%20portfolio%20and"
+                        >
+                            <button className='flex items-center space-x-2 text-lg bg-white px-13 md:px-5 py-2 hover:scale-105 duration-300 transition-all cursor-pointer'>
+                                <p>Text me on</p>
+                                <IoLogoWhatsapp className='text-green-700'/>
+                            </button>
+                        </a>
+
+                    </div>
+                </div>
+            </section>
+        </main>
+    )
+}
+
+export default CallToAction
