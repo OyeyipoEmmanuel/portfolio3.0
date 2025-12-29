902dec5257cdab307f8140dc1f02f75624099573
Emmanuel <emmasco0207@gmail.com>
2025-12-29 10:21:17 +0100

    project section updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import About from "@/components/sections/About";
 import HomePg from "@/components/sections/Home"
+import Projects from "@/components/sections/Projects";
 export default function Home() {
   return (
-    <div className="pt-24 max-w-3xl mx-auto px-4 md:max-w-6xl">
+    <div className="pt-32 max-w-3xl mx-auto px-4 md:max-w-6xl">
       <HomePg/>
       <About/>
+      <Projects/>
     </div>
   );
 }
