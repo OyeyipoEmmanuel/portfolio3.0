7d771994f17d67d4aaff84222035b688fac86336
Emmanuel <emmasco0207@gmail.com>
2025-12-31 19:00:50 +0100

    fix: seo and cleanuo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,68 @@
-'use client';
-
 import "./globals.css";
 import { switzerFont } from "./src/fonts/myfonts";
 import Navbar from "@/components/layout/navbar";
-import { useLayoutEffect } from "react";
-import gsap from "gsap";
-import ScrollTrigger from "gsap/ScrollTrigger";
-import ScrollSmoother from "gsap/ScrollSmoother";
 
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+// app/layout.tsx
+export const metadata = {
+  title: {
+    default: "Emmanuel Oyeyipo – Frontend Engineer",
+    template: "%s | Emmanuel Oyeyipo",
+  },
+  description:
+    "Emmanuel Oyeyipo is a Frontend Engineer dedicated to turning complex ideas into seamless, high-performance digital experiences. With over 2 years of experience, he specialize in building scalable web applications using React, Next.js, and TypeScript.",
+  keywords: [
+    "Frontend Developer",
+    "Frontend Engineer",
+    "React Developer",
+    "Next.js Developer",
+    "JavaScript Engineer",
+    "JavaScript Developer",
+    "Typescript Developer",
+    "Typescript Engineer",
+    "Web Developer",
+    "Frontend Developer Portfolio",
+    "Frontend Engineer Portfolio",
+    "A teenage web developer Portfolio",
+    "Web Developer Portfolio",
+  ],
+  authors: [{ name: "Emmanuel Oyeyipo" }],
+  creator: "Emmanuel Oyeyipo",
+  metadataBase: new URL("https://devemmanuel.tech"),
+  twitter: {
+    card: "summary_large_image",
+    site: "@Emma_Leo01",
+    creator: "@Emma_Leo01",
+    title: "Emmanuel Oyeyipo – Frontend Engineer",
+    description:
+      "Frontend Engineer dedicated to turning complex ideas into seamless, high-performance digital experiences.",
+    images: ["/assets/images/og-image.jpeg"],
+  },
+  openGraph: {
+    title: "Emmanuel Oyeyipo – Frontend Engineer",
+    description:
+      "Frontend Engineer dedicated to turning complex ideas into seamless, high-performance digital experiences.",
+    url: "https://devemmanuel.tech",
+    siteName: "Emmanuel Oyeyipo Portfolio",
+    images: [
+      {
+        url: "/assets/images/og-image.jpeg",
+        width: 1200,
+        height: 630,
+        alt: "Emmanuel Oyeyipo – Frontend Engineer",
+      },
+    ],
+    type: "website",
+  },
+};
+
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  useLayoutEffect(() => {
-    if (ScrollSmoother.get()) return;
-
-    const smoother = ScrollSmoother.create({
-      wrapper: "#smooth-wrapper",
-      content: "#smooth-content",
-      smooth: 1.5,
-      effects: true,
-    });
-
-    return () => smoother.kill();
-  }, []);
-
   return (
     <html lang="en">
       <body className={`antialiased ${switzerFont.variable} scroll-smooth`}>
         <Navbar />
         <div id="smooth-wrapper">
           <div id="smooth-content">
-
             {children}
           </div>
         </div>
