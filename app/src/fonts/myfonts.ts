8b55efce42a1b1e1d3da63b794178947bb0e604d
Emmanuel <emmasco0207@gmail.com>
2025-12-31 20:54:53 +0100

    fix: build fixes

--- app/src/fonts/myfonts.ts
+++ app/src/fonts/myfonts.ts
@@ -3,17 +3,17 @@ import localFont from "next/font/local"
 export const switzerFont = localFont({
     src: [
         {
-            path: "../../../public//fonts/Switzer-Black.woff2",
+            path: "/fonts/Switzer-Black.woff2",
             weight: "400",
             style: "normal"
         },
         {
-            path: "../../../public//fonts/Switzer-Regular.woff2",
+            path: "/fonts/Switzer-Regular.woff2",
             weight: "400",
             style: "normal"
         },
         {
-            path: "../../../public//fonts/Switzer-SemiBold.woff2",
+            path: "/fonts/Switzer-SemiBold.woff2",
             weight: "700",
             style: "normal"
         },
