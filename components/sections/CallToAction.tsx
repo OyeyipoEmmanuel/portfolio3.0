9f6af99b631353f0afc9d15bc9f03eaa311c91bd
Emmanuel <emmasco0207@gmail.com>
2025-12-31 19:01:17 +0100

    fix: seo and cleanuo

--- components/sections/CallToAction.tsx
+++ components/sections/CallToAction.tsx
@@ -1,11 +1,37 @@
-import React from 'react'
+'use client'
+import { useLayoutEffect, useRef } from 'react'
 import { SiGmail } from 'react-icons/si'
 import { IoLogoWhatsapp } from "react-icons/io";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
+gsap.registerPlugin(ScrollTrigger);
 
 const CallToAction = () => {
+    const mainRef = useRef<HTMLElement | null>(null);
+
+    useLayoutEffect(() => {
+        const ctx = gsap.context(() => {
+            gsap.to(mainRef.current, {
+                scale: 1.05,
+                duration: 1,
+                repeat: -1,
+                yoyo: true,
+                ease: "power1.inOut",
+                scrollTrigger: {
+                    trigger: mainRef.current,
+                    start: "top 75%",
+                }
+            });
+        }, mainRef);
+
+        return () => ctx.revert();
+    }, []);
+
+
+
     return (
-        <main className="mt-24 bg-[url('/assets/images/background2.webp')] bg-cover bg-repeat-space bg-center rounded-md">
+        <main ref={mainRef} className="mt-24 bg-[url('/assets/images/background2.webp')] bg-cover bg-repeat-space bg-center rounded-md">
             <section className='flex items-center justify-center mx-auto min-h-[300px]'>
                 <div>
                     <h1 className='text-white font-semibold tracking-tighter text-2xl text-center md:text-4xl font-barlow'>You've come a long way!</h1>
@@ -24,7 +50,7 @@ const CallToAction = () => {
                         >
                             <button className='flex items-center space-x-2 text-lg bg-white px-13 md:px-5 py-2 hover:scale-105 duration-300 transition-all cursor-pointer'>
                                 <p>Text me on</p>
-                                <IoLogoWhatsapp className='text-green-700'/>
+                                <IoLogoWhatsapp className='text-green-700' />
                             </button>
                         </a>
 
