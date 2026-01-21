65896100ae7aa91eadf262b400e2b7c87a0bad0c
Emmanuel <emmasco0207@gmail.com>
2026-01-21 13:51:35 +0100

    feat: education & voluntering section added

---
+++ data/education.ts
@@ -0,0 +1,17 @@
+export const education = [
+    {
+        degree: "BSc in Computer Sciences",
+        school: "University of Lagos",
+        date: "Nov 2024 - Aug 2028"
+    },
+    {
+        degree: "HatchDev 3.1 SWE Training",
+        school: "NitHub",
+        date: "May 2025 - Nov 2025"
+    },
+    {
+        degree: "Diploma in Web Development",
+        school: "NIIT",
+        date: "Nov 2022 - Jan 2024"
+    },
+]
