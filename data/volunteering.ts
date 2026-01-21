a43e4d0d56641d7554aacb2f024e67659cf50ec2
Emmanuel <emmasco0207@gmail.com>
2026-01-21 13:51:52 +0100

    feat: education & voluntering section added

---
+++ data/volunteering.ts
@@ -0,0 +1,14 @@
+export const volunteering = [
+    {
+        role: "Development Team Member",
+        company: "GDG on Campus UNILAG",
+        date: "Jan 2026 - Present",
+        logo:"/assets/images/gdglogo.avif"
+    },
+    {
+        role: "Speakers Support",
+        company: "DevFest Lagos 2025",
+        date: "Nov 2025",
+        logo:"/assets/images/gdglogo.avif"
+    },
+]
