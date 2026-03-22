c22ffdb041ab80e6beac248063a677d2a0182d1f
Emmanuel <emmasco0207@gmail.com>
2026-03-22 18:14:14 +0100

    feat: adding more projects

--- components/ui/ProjectsCard.tsx
+++ components/ui/ProjectsCard.tsx
@@ -122,52 +122,55 @@ const ProjectCard = ({ project }: { project: ProjectsDataType }) => {
 
 
     return (
-        <div ref={cardRef} className="w-full rounded-[20px] overflow-hidden p-5 border-[1.5px] border-[#0000001a] relative transition-all ">
+        <div ref={cardRef} className="w-full grid grid-cols-1  gap-y-6 md:gap-y-3 rounded-[20px] overflow-hidden p-5 border-[1.5px] border-[#0000001a] relative transition-all ">
             <div
                 ref={imageContainerRef}
                 className="group relative w-full border border-[#00000015] rounded-[20px] overflow-hidden"
                 onMouseEnter={() => {
+                    if (project.videoSrc == null) return;
                     const video = videoRef.current;
-                    if (!video) return;
+                    if (!project.videoSrc && !video) return;
 
                     // load source only on hover
-                    if (!video.src) {
-                        video.src = project.videoSrc;
+                    if (video && !video.src) {
+                        video.src = project.videoSrc || "";
                         video.load();
                     }
-                    video.play();
+                    video!.play();
                 }}
                 onMouseLeave={() => {
+                    if (project.videoSrc == null) return;
+
                     const video = videoRef.current;
                     if (!video) return;
 
                     video.pause();
                     video.currentTime = 0;
                 }}
-                //Video play For mobile view
+            //Video play For mobile view
             >
                 <Image
                     width={800}
                     height={600}
                     src={project.imageSrc}
                     alt={project.header + " Preview"}
-                    className="inset-0 w-full h-full object-cover z-10 transition-opacity duration-1000 group-hover:opacity-0"
+                    className={`inset-0 w-full h-full object-cover z-10 transition-opacity duration-1000 ${project.videoSrc && "group-hover:opacity-0"}`}
                 />
-                <video
+                {project.videoSrc && <video
                     ref={videoRef}
                     muted
                     loop
                     preload="none" // important: don't fetch anything until hover
                     className="absolute inset-0 w-full h-full object-center z-0 opacity-0 group-hover:opacity-100 transition-all duration-1000 group-hover:scale-105"
-                />
+                />}
             </div>
 
-            <div className="flex justify-end mt-1">
+           <div className={`flex justify-end ${project.videoSrc == null && " invisible"}`}>
                 <p className="text-black font-light text-[15px] md:hidden">Tap for video preview</p>
                 <p className="text-black font-light text-[15px] hidden md:block">Hover for video preview</p>
             </div>
 
-            <div className='mt-4 flex flex-col space-y-3'>
+            <div className='flex flex-col space-y-3'>
                 <h1 ref={titleRef} className='text-[24px] font-semibold mb-2'>{project.header}</h1>
                 <span ref={descriptionRef} className=''>
                     <p
@@ -184,25 +187,25 @@ const ProjectCard = ({ project }: { project: ProjectsDataType }) => {
                         <p className='underline text-purple-800 cursor-pointer font-semibold' onClick={handleShowMore}>show less</p>
                     )}
                 </span>
-
-                <div ref={stackRef} className="grid grid-cols-2 md:grid-cols-3 gap-2 mt-4 min-h-[80px]">
-                    {project.stack.map((stack, idx) => (
-                        <span key={idx} className="px-4 group relative overflow-hidden py-2 border-2 h-fit border-black text-sm font-bold uppercase hover:text-white tracking-wide before:absolute before:inset-0 before:w-full before:h-full before:bg-black before:translate-y-[100%] hover:before:translate-y-0 before:transition-transform before:duration-500">
-                            <p className="relative group-hover:text-white z-10 text-[13px] md:text-md">{stack}</p>
-                        </span>
-                    ))}
-                </div>
             </div>
 
-            <div ref={buttonsRef} className="flex items-center space-x-3 text-white mt-8 pt-4 border-t border-[#00000041]">
-                <Link href={project.liveLink} target="_blank">
-                    <ButtonUI className="px-10 md:px-14 py-2 flex space-x-1 hover:md:px-15 transition-all duration-300">
+            <div ref={stackRef} className="grid grid-cols-2 md:grid-cols-3 gap-2 min-h-[80px]">
+                {project.stack.map((stack, idx) => (
+                    <span key={idx} className="px-4 group relative overflow-hidden py-2 border-2 h-fit border-black text-sm font-bold uppercase hover:text-white tracking-wide before:absolute before:inset-0 before:w-full before:h-full before:bg-black before:translate-y-[100%] hover:before:translate-y-0 before:transition-transform before:duration-500">
+                        <p className="relative group-hover:text-white z-10 text-[12px] tracking-tighter md:text-md">{stack}</p>
+                    </span>
+                ))}
+            </div>
+
+            <div ref={buttonsRef} className="flex items-center space-x-3 text-white pt-4 border-t border-[#00000041]">
+                {project.liveLink !== "" && <Link href={project.liveLink} target="_blank">
+                    <ButtonUI className="px-10 md:px-10 py-2 flex space-x-1 hover:md:px-15 transition-all duration-300">
                         <p className="tracking-wide md:text-lg">Visit</p>
                         <BiLinkExternal className="text-xs" />
                     </ButtonUI>
-                </Link>
+                </Link>}
                 <Link href={project.githubLink} target="_blank">
-                    <button className="cursor-pointer px-4 text-black group relative overflow-hidden py-2 border-2 h-fit border-black text-sm font-bold uppercase hover:text-white tracking-wide before:absolute before:inset-0 before:w-full before:h-full before:bg-black before:translate-y-[100%] hover:before:translate-y-0 before:transition-transform before:duration-500 flex ">
+                    <button className="cursor-pointer px-4 text-black group relative overflow-hidden py-[7px] border-2 h-fit border-black text-sm font-bold uppercase hover:text-white tracking-wide before:absolute before:inset-0 before:w-full before:h-full before:bg-black before:translate-y-[100%] hover:before:translate-y-0 before:transition-transform before:duration-500 flex">
                         <p className="relative group-hover:text-white z-10">Github</p>
                         <BiLinkExternal className="relative group-hover:text-white z-10" />
                     </button>
