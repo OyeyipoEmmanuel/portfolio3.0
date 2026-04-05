02e20c5afc4de276c1ba40ad7cb653e6ee4c1e5a
Emmanuel <emmasco0207@gmail.com>
2026-04-05 08:34:08 +0100

    Fixing web accessibility

--- components/sections/Education.tsx
+++ components/sections/Education.tsx
@@ -8,11 +8,11 @@ const Education = () => {
     return (
         <main className='mt-24 mb-12 pt-12 border-t border-[#00000015]'>
             <section className='flex items-start space-x-1'>
-                <h1 className="text-[32px] tracking-[-.03em] md:text-3xl leading-[110%] font-semibold">
+                <h2 className="text-[32px] tracking-[-.03em] md:text-3xl leading-[110%] font-semibold">
                     Education
-                </h1>
+                </h2>
                 <span>
-                    <PiGraduationCapFill className='text-[26px] rotate-45' />
+                    <PiGraduationCapFill className='text-[26px] rotate-45' aria-hidden="true" />
                 </span>
             </section>
 
@@ -20,12 +20,12 @@ const Education = () => {
                 {education.map((each, idx) => (
                     <div key={idx} className='border border-[#00000015] rounded-lg w-full p-4 flex flex-row-reverse md:flex-row space-x-4 items-center justify-between hover:-translate-y-2 duration-300 transition-all hover:shadow-lg' data-aos="fade-left">
                         <div className='w-[5%]'>
-                            <FaGraduationCap className='text-2xl' />
+                            <FaGraduationCap className='text-2xl' aria-hidden="true" />
                         </div>
                         <div className='flex flex-col space-y-4 w-[90%] md:w-[60%]'>
                             <span className='flex flex-col space-y-1'>
-                                <h1 className='text-xl tracking-tighter font-semibold'>{each.degree}</h1>
-                                <h3 className='text-[#4287F6] font-light text-lg'>{each.school}</h3>
+                                <h3 className='text-xl tracking-tighter font-semibold'>{each.degree}</h3>
+                                <p className='text-[#4287F6] font-light text-lg'>{each.school}</p>
                             </span>
 
                             <p className='text-gray-500 md:hidden'>{each.date}</p>
